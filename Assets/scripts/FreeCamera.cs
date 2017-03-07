fa45248137ca9f369f32494a704a093013ae7e13
kurtLab <kurtportelli@gmail.com>
2017-03-07 18:51:45 +0000

    added table

---
+++ Assets/scripts/FreeCamera.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeCamera : MonoBehaviour {
+
+	public float flySpeed = 0.5f;
+	public GameObject defaultCam;
+	public float accelerationAmount = 3f;
+	public float accelerationRatio = 1f;
+	public float slowDownRatio = 0.5f;
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+		{
+			flySpeed *= accelerationRatio;
+		}
+
+		if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
+		{
+			flySpeed /= accelerationRatio;
+		}
+		if (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl))
+		{
+			flySpeed *= slowDownRatio;
+		}
+		if (Input.GetKeyUp(KeyCode.LeftControl) || Input.GetKeyUp(KeyCode.RightControl))
+		{
+			flySpeed /= slowDownRatio;
+		}
+		if (Input.GetAxis("Vertical") != 0)
+		{
+			transform.Translate(-defaultCam.transform.forward * flySpeed * Input.GetAxis("Vertical"));
+		}
+		if (Input.GetAxis("Horizontal") != 0)
+		{
+			transform.Translate(-defaultCam.transform.right * flySpeed * Input.GetAxis("Horizontal"));
+		}
+		if (Input.GetKey(KeyCode.E))
+		{
+			transform.Translate(defaultCam.transform.up * flySpeed*0.5f);
+		}
+		else if (Input.GetKey(KeyCode.Q))
+		{
+			transform.Translate(-defaultCam.transform.up * flySpeed*0.5f);
+		}
+	}
+}
---
+++ Assets/scripts/MouseLook.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+	float ScrollSpeed = 15f;
+	float ScrollEdge = 0.01f;
+
+	private int HorizontalScroll = 1;
+	private int VerticalScroll = 1;
+	private int DiagonalScroll = 1;
+
+	float PanSpeed = 10f;
+
+	Vector2 ZoomRange = new Vector2(-10,10);
+	float CurrentZoom = 0f;
+	float ZoomZpeed = 1f;
+	float ZoomRotation = 1f;
+
+	private Vector3 InitPos;
+	private Vector3 InitRotation;
+
+
+
+	void Start()
+	{
+		//Instantiate(Arrow, Vector3.zero, Quaternion.identity);
+
+		InitPos = transform.position;
+		InitRotation = transform.eulerAngles;
+
+	}
+
+	void Update ()
+	{
+		//PAN
+		if ( Input.GetKey("mouse 2") )
+		{
+			//(Input.mousePosition.x - Screen.width * 0.5)/(Screen.width * 0.5)
+
+			transform.Translate(Vector3.right * Time.deltaTime * PanSpeed * (Input.mousePosition.x - Screen.width * 0.5f)/(Screen.width * 0.5f), Space.World);
+			transform.Translate(Vector3.forward * Time.deltaTime * PanSpeed * (Input.mousePosition.y - Screen.height * 0.5f)/(Screen.height * 0.5f), Space.World);
+
+		}
+		else
+		{
+			if ( Input.GetKey("d") || Input.mousePosition.x >= Screen.width * (1 - ScrollEdge) )
+			{
+				transform.Translate(Vector3.right * Time.deltaTime * ScrollSpeed, Space.World);
+			}
+			else if ( Input.GetKey("a") || Input.mousePosition.x <= Screen.width * ScrollEdge )
+			{
+				transform.Translate(Vector3.right * Time.deltaTime * -ScrollSpeed, Space.World);
+			}
+
+			if ( Input.GetKey("w") || Input.mousePosition.y >= Screen.height * (1 - ScrollEdge) )
+			{
+				transform.Translate(Vector3.forward * Time.deltaTime * ScrollSpeed, Space.World);
+			}
+			else if ( Input.GetKey("s") || Input.mousePosition.y <= Screen.height * ScrollEdge )
+			{
+				transform.Translate(Vector3.forward * Time.deltaTime * -ScrollSpeed, Space.World);
+			}
+		}
+
+		//ZOOM IN/OUT
+
+		CurrentZoom -= Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime * 1000 * ZoomZpeed;
+
+		CurrentZoom = Mathf.Clamp(CurrentZoom,ZoomRange.x,ZoomRange.y);
+
+		transform.position = new Vector3 (transform.position.x,transform.position.y - (transform.position.y - (InitPos.y + CurrentZoom)) * 0.1f,transform.position.z);
+		transform.eulerAngles = new Vector3 (transform.eulerAngles.x - (transform.eulerAngles.x - (InitRotation.x + CurrentZoom * ZoomRotation)) * 0.1f,transform.eulerAngles.y, transform.eulerAngles.z);
+
+	}
+}
