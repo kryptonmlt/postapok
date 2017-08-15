cd31126182e008c865c4fea458882c91056c1b62
Kurt Portelli <kurtportelli@gmail.com>
2017-08-15 13:57:11 +0100

    added camera view restrictions

--- Assets/scripts/MouseLook.cs
+++ Assets/scripts/MouseLook.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseLook : MonoBehaviour {
+public class MouseLook : MonoBehaviour
+{
 	float ScrollSpeed = 15f;
 	float ScrollEdge = 0.01f;
 
 	float PanSpeed = 10f;
 
-	Vector2 ZoomRange = new Vector2(-10,10);
+	Vector2 ZoomRange = new Vector2 (-10, 10);
 	float CurrentZoom = 0f;
 	float ZoomZpeed = 1f;
 	float ZoomRotation = 1f;
@@ -18,7 +19,7 @@ public class MouseLook : MonoBehaviour {
 
 
 
-	void Start()
+	void Start ()
 	{
 		//Instantiate(Arrow, Vector3.zero, Quaternion.identity);
 
@@ -30,43 +31,34 @@ public class MouseLook : MonoBehaviour {
 	void Update ()
 	{
 		//PAN
-		if ( Input.GetKey("mouse 2") )
-		{
+		if (Input.GetKey ("mouse 2")) {
 			//(Input.mousePosition.x - Screen.width * 0.5)/(Screen.width * 0.5)
 
-			transform.Translate(Vector3.right * Time.deltaTime * PanSpeed * (Input.mousePosition.x - Screen.width * 0.5f)/(Screen.width * 0.5f), Space.World);
-			transform.Translate(Vector3.forward * Time.deltaTime * PanSpeed * (Input.mousePosition.y - Screen.height * 0.5f)/(Screen.height * 0.5f), Space.World);
+			transform.Translate (Vector3.right * Time.deltaTime * PanSpeed * (Input.mousePosition.x - Screen.width * 0.5f) / (Screen.width * 0.5f), Space.World);
+			transform.Translate (Vector3.forward * Time.deltaTime * PanSpeed * (Input.mousePosition.y - Screen.height * 0.5f) / (Screen.height * 0.5f), Space.World);
 
-		}
-		else
-		{
-			if ( Input.GetKey("d") || Input.mousePosition.x >= Screen.width * (1 - ScrollEdge) )
-			{
-				transform.Translate(Vector3.right * Time.deltaTime * ScrollSpeed, Space.World);
-			}
-			else if ( Input.GetKey("a") || Input.mousePosition.x <= Screen.width * ScrollEdge )
-			{
-				transform.Translate(Vector3.right * Time.deltaTime * -ScrollSpeed, Space.World);
+		} else {
+			if ((Input.GetKey ("d") || Input.mousePosition.x >= Screen.width * (1 - ScrollEdge)) && transform.position.x < 50) {
+				transform.Translate (Vector3.right * Time.deltaTime * ScrollSpeed, Space.World);
+			} else if ((Input.GetKey ("a") || Input.mousePosition.x <= Screen.width * ScrollEdge) && transform.position.x > -50) {
+				transform.Translate (Vector3.right * Time.deltaTime * -ScrollSpeed, Space.World);
 			}
 
-			if ( Input.GetKey("w") || Input.mousePosition.y >= Screen.height * (1 - ScrollEdge) )
-			{
-				transform.Translate(Vector3.forward * Time.deltaTime * ScrollSpeed, Space.World);
-			}
-			else if ( Input.GetKey("s") || Input.mousePosition.y <= Screen.height * ScrollEdge )
-			{
-				transform.Translate(Vector3.forward * Time.deltaTime * -ScrollSpeed, Space.World);
+			if ((Input.GetKey ("w") || Input.mousePosition.y >= Screen.height * (1 - ScrollEdge)) && transform.position.z < 40) {
+				transform.Translate (Vector3.forward * Time.deltaTime * ScrollSpeed, Space.World);
+			} else if ((Input.GetKey ("s") || Input.mousePosition.y <= Screen.height * ScrollEdge) && transform.position.z > -70) {
+				transform.Translate (Vector3.forward * Time.deltaTime * -ScrollSpeed, Space.World);
 			}
 		}
 
 		//ZOOM IN/OUT
 
-		CurrentZoom -= Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime * 1000 * ZoomZpeed;
+		CurrentZoom -= Input.GetAxis ("Mouse ScrollWheel") * Time.deltaTime * 1000 * ZoomZpeed;
 
-		CurrentZoom = Mathf.Clamp(CurrentZoom,ZoomRange.x,ZoomRange.y);
+		CurrentZoom = Mathf.Clamp (CurrentZoom, ZoomRange.x, ZoomRange.y);
 
-		transform.position = new Vector3 (transform.position.x,transform.position.y - (transform.position.y - (InitPos.y + CurrentZoom)) * 0.1f,transform.position.z);
-		transform.eulerAngles = new Vector3 (transform.eulerAngles.x - (transform.eulerAngles.x - (InitRotation.x + CurrentZoom * ZoomRotation)) * 0.1f,transform.eulerAngles.y, transform.eulerAngles.z);
+		transform.position = new Vector3 (transform.position.x, transform.position.y - (transform.position.y - (InitPos.y + CurrentZoom)) * 0.1f, transform.position.z);
+		transform.eulerAngles = new Vector3 (transform.eulerAngles.x - (transform.eulerAngles.x - (InitRotation.x + CurrentZoom * ZoomRotation)) * 0.1f, transform.eulerAngles.y, transform.eulerAngles.z);
 
 	}
 }
